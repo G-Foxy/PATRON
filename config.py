dc81f61881650ed412074c8ea3cca12f669508e6
G-Foxy <166919890+G-Foxy@users.noreply.github.com>
2024-04-13 16:55:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Hebun2121/musivvv-",
+    "https://github.com/G-Foxy/PATRON",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
